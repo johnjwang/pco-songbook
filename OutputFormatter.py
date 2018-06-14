8c536ef373172b4d0f4025aaffd3e0d5ad9c7ed1
Alexander Wang <gnawang@umich.edu>
2018-06-14 19:11:20 -0400

    indentations

--- OutputFormatter.py
+++ OutputFormatter.py
@@ -5,6 +5,13 @@ CHORD_FONT = 'Arial'
 LYRIC_SIZE = 12
 LYRIC_FONT = 'Arial'
 
+INDENT_SIZE = 4
+INDENT_PARTS = [
+    'pre-chorus',
+    'chorus',
+    'bridge',
+]
+
 
 class SongbookPDF(fpdf.FPDF):
 
@@ -26,7 +33,7 @@ class SongbookPDF(fpdf.FPDF):
     def print_title(self, num, title):
         self.set_font('Arial', 'B', 12)
         self.set_fill_color(200, 220, 255)
-        self.cell(0, 6, num + ' - ' + title, 0, 1, 'L', 0)
+        self.cell(0, 6, num + '\t - \t' + title, 0, 1, 'L', 0)
 
     def get_chord_width(self, chord):
         self.set_font(CHORD_FONT, '', CHORD_SIZE)
@@ -36,12 +43,10 @@ class SongbookPDF(fpdf.FPDF):
         self.set_font(LYRIC_FONT, '', LYRIC_SIZE)
         return self.get_string_width(lyric)
 
-    def print_line(self, left_margin, line):
-        self.set_x(left_margin)
-
+    def print_line(self, line):
         # Print out first lyric segment, may be empty
         if line.lyrics[0]:
-            self.set_y(self.get_y() + self.cho_height)
+            self.set_xy(self.get_x(), self.get_y() + self.cho_height)
 
             self.set_font(LYRIC_FONT, '', LYRIC_SIZE)
             self.cell(self.get_string_width(
@@ -63,19 +68,30 @@ class SongbookPDF(fpdf.FPDF):
             self.cell(self.get_string_width(
                 line.lyrics[i + 1]), self.font_size, line.lyrics[i + 1])
 
-            self.set_y(self.get_y() - self.cho_height)
+            self.set_xy(self.get_x(), self.get_y() - self.cho_height)
             x_next = lyric_x if lyric_x > chord_x else chord_x
 
         self.ln(self.cho_height + self.lyr_height)
 
-    def print_part(self, lines):
-        for line in lines:
-            self.print_line(self.l_margin, line)
-        self.ln(self.cho_height)
+    def print_part(self, indent, part, lines):
+        if lines:
+            for line in lines:
+                self.set_x(indent)
+                self.print_line(line)
+            self.ln(self.cho_height)
+        else:
+            self.set_x(indent)
+            self.set_font(CHORD_FONT, '', CHORD_SIZE)
+            self.cell(self.get_string_width(part), self.font_size, part, ln=1)
 
     def print_song(self, song):
         self.add_page()
         self.print_title(song.key, song.title)
 
+        indent_count = 0
         for part, lines in song.chord_chart.items():
-            self.print_part(lines)
+            indent = self.l_margin
+            if part.lower() in INDENT_PARTS:
+                indent_count += 1
+                indent += indent_count * INDENT_SIZE
+            self.print_part(indent, part, lines)

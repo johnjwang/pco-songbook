7ab1648240cb9fef443c036e02b79b9072e1fdd5
John Wang <jnwang@umich.edu>
2018-06-17 17:38:01 -0400

    remove camel case

--- songbook/generator.py
+++ songbook/generator.py
@@ -9,28 +9,28 @@ from song import Song
 TITLE_MARKER = ' - '
 
 
-def getSongData(ID):
+def get_song_data(ID):
     # TODO: Hide these keys somewhere
     head = {
         "Authorization": "Basic MmVhYTc2NTVkYzExZDFjNzFhODI5NmQ2ODkyMmE0MTAwOTkxZDQ2NmNjYzM1ZmJhOWZjOGMxZWQyZDI5MWUxZjphMmZhZWEyNDc1MmZhMTRjYzEzM2UzNjRlMmFjM2IzMzEyYmI5OWEzYWY0ZTEyNTEzODg2NzJjZTQ4ZTNlZmYy"
     }
-    songReq = requests.get(
+    song_req = requests.get(
         'https://api.planningcenteronline.com/services/v2/songs/{0}/arrangements'.format(ID), headers=head)
-    songObj = json.loads(songReq.text)
+    song_obj = json.loads(song_req.text)
 
-    return songObj["data"][0]["attributes"]["chord_chart"]
+    return song_obj["data"][0]["attributes"]["chord_chart"]
 
 def generate_pdf(infilename, outfilename):
-    songsCSV = pandas.read_csv(infilename)
+    songs_csv = pandas.read_csv(infilename)
 
     # Retreive and ingest the song data
     songbook = {}
-    for song in songsCSV.itertuples():
+    for song in songs_csv.itertuples():
         ID = int(song[1])
         title_data = str(song[2]).split(TITLE_MARKER)
 
         # TODO: Get publishing company somehow
-        songbook[ID] = Song(title_data[0], title_data[1], getSongData(ID))
+        songbook[ID] = Song(title_data[0], title_data[1], get_song_data(ID))
 
     # Output the songdata
     book = output.SongbookPDF()
